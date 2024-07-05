7af7bf6c6fe80c2856f78abe552bc4526ea9dd4e
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2024-07-05 21:44:02 +0530

    other ES6 features spread,destructuring added

---
+++ main.js
@@ -0,0 +1,6 @@
+// SPREAD OPERATOR (...)
+//Combining Arrays
+const arr1 = [1,2,3,4,5]
+const arr2 = [8,9,10,11,12]
+
+console.log(...arr1,6,7,...arr2);
---
+++ sub.js
@@ -0,0 +1,12 @@
+//ARRAY DESTRUCTURING
+
+//SWAPPING VARIABLES VALUES USING DESTRUCTURING
+
+let a = 10;
+let b = 20;
+
+ [b,a] = [a,b]
+ console.log(a);
+ console.log(b);
+
+ const {handleSubmit,register} = useForm()  //-----> useForm return de structured into values
