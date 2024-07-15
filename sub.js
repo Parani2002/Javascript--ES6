41c18b3e2a42819455b31f3c49f9aa889a1f0b64
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2024-07-15 18:44:48 +0530

    object destructuring added

--- sub.js
+++ sub.js
@@ -9,4 +9,35 @@ let b = 20;
  console.log(a);
  console.log(b);
 
- const {handleSubmit,register} = useForm()  //-----> useForm return de structured into values
+ const numbers = [1, 2, 3, 4, 5];
+
+// Using the rest operator to collect remaining items
+const [first, second, ...rest] = numbers;
+
+console.log(first);  
+console.log(second); 
+console.log(rest);   
+
+
+//  const {handleSubmit,register} = useForm()  //-----> useForm return de structured into values  Object destructuring
+
+ const [x,y,z] = [2,3,4]
+ console.log(x,y,z); 
+
+ //OBJECT DESTRUCTURING
+
+ const user = {
+    firstname:"Paranitharan",
+    lastname:"Suntharalingam",
+    age:22,
+    place:"Jaffna",
+    degreeDetails: {degree:"BEng(Hons) Software Engineering", institute:"Informatics Institute of Technology"}
+ }
+
+
+ const objectReturn = () => user   //It returns user object
+
+ const {firstname,lastname,...p} = objectReturn()  //Object destructuring
+ console.log(firstname);
+ console.log(lastname);
+ console.log(p);
